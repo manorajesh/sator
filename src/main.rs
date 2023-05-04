b0edd60fcf795f24bc44b135e4e0be9af88b623b
manorajesh <mano@raje.sh>
2023-05-04 14:25:48 -0700

    Complete square

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let wordlist = std::fs::read_to_string("words_alpha.txt").unwrap();
     let words: Vec<&str> = wordlist.lines().collect();
 
-    let mut square: Vec<&str> = Vec::new();
+    let mut square: Vec<String> = Vec::new();
     let mut idx: usize = 1;
 
     'main: for master_word in &words {
@@ -11,8 +11,8 @@ fn main() {
             continue;
         }
         
-        square.push(master_word);
-        'small: for child_word in &words {
+        square.push(master_word.to_string());
+        for child_word in &words {
             if child_word.len() != master_word.len() {
                 continue;
             }
@@ -21,23 +21,30 @@ fn main() {
                 continue;
             }
 
-            if master_word.chars().nth(master_word.len()-idx).expect(format!("idx: {}, mword: {}, cword: {}", idx, master_word, child_word).as_str()) != child_word.chars().nth(child_word.len()-1).unwrap() {
+            if master_word.chars().nth(master_word.len()-idx-1).expect(format!("idx: {}, mword: {}, cword: {}", idx, master_word, child_word).as_str()) != child_word.chars().nth(child_word.len()-1).unwrap() {
                 continue;
             }
 
-            square.push(child_word);
+            square.push(child_word.to_string());
             idx += 1;
             if square.len() == master_word.len()/2 {
-                print_square(&square);
                 break 'main;
             }
         }
         square.clear();
         idx = 1;
     }
+
+    let mut idx = 1;
+    while square.len() < square[0].len() {
+        let rev_word = square[idx].chars().rev().collect::<String>();
+        square.push(rev_word);
+        idx += 1;
+    }
+    print_square(&square);
 }
 
-fn print_square(square: &Vec<&str>) {
+fn print_square(square: &Vec<String>) {
     for word in square {
         for c in word.chars() {
             print!("{} ", c);
